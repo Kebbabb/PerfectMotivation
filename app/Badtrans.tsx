ddc91e5667dbcdb9489433b7127f27453c3823d3
Kebbabb <benyaminmahi@gmail.com>
2024-12-09 13:56:22 +0100

    Increase redirect timer from 2 seconds to 3 seconds in Badtrans and Greattrans components

--- app/Badtrans.tsx
+++ app/Badtrans.tsx
@@ -5,7 +5,7 @@ export default function Badtrans() {
     const router = useRouter(); //MÅ IKKE FJERNES
       const timer = setTimeout(() => {//MÅ IKKE FJERNES
         router.push('/FrontPage'); //MÅ IKKE FJERNES
-      }, 2000);//MÅ IKKE FJERNES
+      }, 3000);//MÅ IKKE FJERNES
       return () => clearTimeout(timer);//MÅ IKKE FJERNES
     //skriv her fra:
     
