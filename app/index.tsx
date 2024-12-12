61b73615bd4462ee70fff19558d45eec3b8f2ad3
Kebbabb <benyaminmahi@gmail.com>
2024-12-12 13:32:18 +0100

    rrr

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 //denne ændre jeg så ikke rør noget Beny.
-import { Text, View,StyleSheet,Button,TouchableHighlight,TouchableOpacity,Dimensions } from "react-native";
+import { Text, View,StyleSheet,ImageBackground } from "react-native";
 import {Link} from "expo-router";
 const styles = StyleSheet.create({
 
@@ -14,42 +14,43 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   Buttons: {
-    margin: 10,
-    padding: 10,
+    margin: 50,
+    width:100,
+    height: 50,
     backgroundColor: "lightblue",
     color: "white",
     borderRadius: 10,
     textAlign: "center",
-    alignContent: "center",
+    alignItems: "center",
     justifyContent: "center",
   },
+  backgroundImage:{
+    flex:1,
+    resizeMode: "cover",
+    justifyContent:"center",
+    alignItems: "center"
+  }
 
 });
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: -200,
-        backgroundColor: "white",
-      }}
-    >
+    <ImageBackground 
+    source={require('C:/Users/next4937/Documents/React native/perfectmotivation/assets/images/BB.png')}
+    style={styles.backgroundImage}>
       <View>
         <Text style={styles.Maintext}>Hi Benyamin how has your day been!</Text>
       </View>
       <View style={styles.Buttonmargin}>
     
         <View style={styles.Buttons}>
-          <Link href={"/Greattrans"}><Text style={{color:"white",}}>Great</Text></Link>
+          <Link href={"/Greattrans"}><Text style={{color:"white",fontSize:25}}>Great</Text></Link>
         </View>
 
         <View style={styles.Buttons}>
-          <Link href={"/Badtrans"}><Text style={{color:"white",}}>Bad</Text></Link>
+          <Link href={"/Badtrans"}><Text style={{color:"white", fontSize:25}}>Bad</Text></Link>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
