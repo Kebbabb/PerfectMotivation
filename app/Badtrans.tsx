fb8a903b9c3249c0a13381bcc1a8dc0298c46f4c
Kebbabb <benyaminmahi@gmail.com>
2024-12-12 12:34:23 +0100

    ready

--- app/Badtrans.tsx
+++ app/Badtrans.tsx
@@ -1,12 +1,40 @@
 //alt det, der har en grøn tekst foran må ikke fjernes ellers gør hvad du vil fpr at gøre den flot
-import { useRouter } from 'expo-router';//må ikkeq fjernes
+import React, { useEffect } from 'react';
+import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Badtrans() {
-    const router = useRouter(); //MÅ IKKE FJERNES
-      const timer = setTimeout(() => {//MÅ IKKE FJERNES
-        router.push('/FrontPage'); //MÅ IKKE FJERNES
-      }, 3000);//MÅ IKKE FJERNES
-      return () => clearTimeout(timer);//MÅ IKKE FJERNES
-    //skriv her fra:
-    
-}
+
+const Badtrans = () => {
+  const router = useRouter(); // MÅ IKKE FJERNES
+
+  useEffect(() => {
+    const timer = setTimeout(() => { // MÅ IKKE FJERNES
+      router.push('/FrontPage'); // MÅ IKKE FJERNES
+    }, 3000); // MÅ IKKE FJERNES
+
+    return () => clearTimeout(timer); // MÅ IKKE FJERNES
+  }, [router]);
+
+  
+  // skriv herinde fra:
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>WOMP WOMP NIGGA!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 100,
+    fontWeight: 'bold',
+  },
+});
+
+export default Badtrans;
--- app/Greattrans.tsx
+++ app/Greattrans.tsx
@@ -1,12 +1,40 @@
 //alt det, der har en grøn tekst foran må ikke fjernes ellers gør hvad du vil fpr at gøre den flot
+import React, { useEffect } from 'react';
 import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Greattrans() {
-    const router = useRouter(); //MÅ IKKE FJERNES
-      const timer = setTimeout(() => {//MÅ IKKE FJERNES
-        router.push('/FrontPage'); //MÅ IKKE FJERNES
-      }, 3000);//MÅ IKKE FJERNES
-      return () => clearTimeout(timer);//MÅ IKKE FJERNES
-    //skriv her fra:
 
-    }
+const Greattrans = () => {
+  const router = useRouter(); // MÅ IKKE FJERNES
+
+  useEffect(() => {
+    const timer = setTimeout(() => { // MÅ IKKE FJERNES
+      router.push('/FrontPage'); // MÅ IKKE FJERNES
+    }, 3000); // MÅ IKKE FJERNES
+
+    return () => clearTimeout(timer); // MÅ IKKE FJERNES
+  }, [router]);
+
+  
+  // skriv herinde fra:
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Great, then let's look at your day for today</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default Greattrans;
